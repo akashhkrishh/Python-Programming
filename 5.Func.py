55f4c2732bca5309f7d00bc7f26191471b5b4d1c
Akashh Krishh <54142695+akashhkrishh@users.noreply.github.com>
2023-02-05 22:25:41 +0530

    Create 5.Func.py

--- 5.Func.py
+++ 5.Func.py
@@ -1,11 +1,11 @@
 def add(a,b):
-    return a+b
+    print("\nAddition of {} + {} = {}\n".format(a,b,a+b))
 def sub(a,b):
-    return a-b
+    print("\nSubraction of {} - {} = {}".format(a,b,a-b))
 def mul(a,b):
-    return a*b
+    print("\nMultiplication of {} * {} = {}".format(a,b,a*b))
 def div(a,b):
-    return a/b
+    print("\nDivision of {} / {} = {:0.2f}".format(a,b,a/b))
 
 a,b = input("Enter the Values A and B :").split(" ")
 a,b = int(a),int(b)
@@ -13,13 +13,13 @@ while(True):
     print("\nChoose the Option 1 to 4 :\n1.Addition \n2.Subraction\n3.Multiplication\n4.Division\n5.Exit")
     ch=int(input("\nEnter the Choice : "))
     if(ch==1):
-        print("\nAddition of {} + {} = {}".format(a,b,add(a,b)))
+        add(a,b)
     elif(ch==2):
-        print("Subraction of {} - {} = {}".format(a,b,sub(a,b)))
+        sub(a,b)
     elif(ch==3):
-        print("\nMultiplication of {} * {} = {}".format(a,b,mul(a,b)))
+        mul(a,b)
     elif(ch==4):
-       print("\nDivision of {} / {} = {}".format(a,b,div(a,b)))
+       div(a,b)
     elif(ch==5):
        break
     else:
